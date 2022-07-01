36b806aa2baa81cfd8051ffa90a5d50443def8ef
ayushakash <ayushakash9@gmai.com>
2022-07-01 12:05:08 +0530

    css added

--- app.js
+++ app.js
@@ -4,20 +4,35 @@ const bodyParser = require("body-parser");
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 var items = [];
+var error;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
 
-    res.render('index', {x: items});
+    res.render('index', {
+        x: items,
+        y: error
+    });
 
 });
 
-app.post('/', (req, res) => {                     //to handle form post request
+app.post('/', (req, res) => { //to handle form post request
 
-    var item = req.body.input;                      //req.body. name of input
-    items.push(item);
-    res.redirect('/');                              //to re render '/' route
+    var item = req.body.input;
+    if (item == 0) {
+
+        error = "please enter data";
+        res.redirect('/');
+
+    } else {
+
+        items.push(item);
+        error = "";
+        res.redirect('/');
+    }
+
+    
 
 });
 
